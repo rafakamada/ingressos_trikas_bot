5eeb0a53099208815070e5056c7932d34dd861d6
Rafael Kamada <rafakamada@gmail.com>
2023-08-25 17:00:28 -0300

    cria arquivo de inputs

---
+++ user_inputs.py
@@ -0,0 +1,12 @@
+# url da página de compra do jogo
+url =
+
+# lista com os setores em ordem de preferência
+grandstand_sections = ["ARQUIBANCADA BITSO LESTE", "ARQUIBANCADA OESTE", "ARQUIBANCADA NORTE", "ARQUIBANCADA SUL"]
+
+#credenciais
+username =
+pasword =
+
+# número de convidados
+guests =
