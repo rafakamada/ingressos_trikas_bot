9fbfebeb4991afed879f9151f068950bb9572523
Rafael Kamada <rafakamada@gmail.com>
2023-08-26 11:31:40 -0300

    nailpolish

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ driver = SeleniumDriver(URL, IS_SCHEDULED, SCHEDULED_TIMESTAMP)
 success = False
 section_index = 0
 
-while not success:
 
+def update_section_index(current_index):
+    if current_index + 1 < len(GRANDSTAND_SECTIONS):
+        return current_index + 1
+    else:
+        return 0
+
+
+while not success:
     driver.accept_cookies()
 
     cpf_success = driver.input_cpf(CPF)
@@ -20,18 +27,12 @@ while not success:
     section_was_found = driver.go_to_section_tab(GRANDSTAND_SECTIONS[section_index])
 
     if not section_was_found:
-        if section_index + 1 < len(GRANDSTAND_SECTIONS):
-            section_index += 1
-        else:
-            section_index = 0
+        section_index = update_section_index(section_index)
         continue
 
     tickets_were_added = driver.add_tickets_to_cart(NUMBER_OF_GUESTS)
     if not tickets_were_added:
-        if section_index + 1 < len(GRANDSTAND_SECTIONS):
-            section_index += 1
-        else:
-            section_index = 0
+        section_index = update_section_index(section_index)
         continue
 
     success = driver.log_in(USERNAME, PASSWORD)
--- selenium_driver.py
+++ selenium_driver.py
@@ -21,13 +21,14 @@ class SeleniumDriver:
         self.driver.maximize_window()
         if is_scheduled:
             time_to_wait = scheduled_start - datetime.now()
-            if time_to_wait.seconds < 80000:  # não fica negativo
+            if scheduled_start > datetime.now():
                 time.sleep(time_to_wait.seconds)
 
     def accept_cookies(self):
         try:
-            cookies_button = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.presence_of_element_located((By.XPATH, "//a[@class='cc-btn cc-dismiss']")))
+            cookies_button = self.wait_and_find_element(method=By.XPATH,
+                                                        timeout=random.randrange(2, 5),
+                                                        element_id_or_xpath="//a[@class='cc-btn cc-dismiss']")
 
             cookies_button.click()
             print("cookies aceitos")
@@ -36,12 +37,15 @@ class SeleniumDriver:
 
     def input_cpf(self, cpf):
         try:
-            cpf_field = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.presence_of_element_located((By.ID, "inputPromocode")))
+            cpf_field = self.wait_and_find_element(method=By.ID,
+                                                   timeout=random.randrange(2, 5),
+                                                   element_id_or_xpath="inputPromocode")
+
             cpf_field.send_keys(cpf)
 
             cpf_field.submit()
 
+            # self.driver.implicitly_wait(15)
             print("cpf ok")
 
             return True
@@ -55,17 +59,19 @@ class SeleniumDriver:
         section_was_found = False
         while not section_was_found:
             try:
-                tab_button = WebDriverWait(self.driver, timeout=random.randrange(5, 10)).until(  # esse demora mesmo
-                    EC.element_to_be_clickable((By.XPATH, f"//button[@title='{section_name}']"))
-                )
+                tab_button = self.wait_and_find_element(method=By.XPATH,
+                                                        timeout=random.randrange(2, 5),
+                                                        element_id_or_xpath=f"//button[@title='{section_name}']")
 
                 tab_button.click()
                 section_was_found = True
                 return section_was_found
             except:
                 try:
-                    next_arrow_button = WebDriverWait(self.driver, timeout=random.randrange(1, 3)).until(
-                        EC.element_to_be_clickable((By.ID, "sector-next")))
+                    next_arrow_button = self.wait_and_find_element(method=By.ID,
+                                                                   timeout=random.randrange(1, 3),
+                                                                   element_id_or_xpath="sector-next")
+
                     next_arrow_button.click()
                 except:
                     print("setor não encontrado. tentando o próximo...")
@@ -74,51 +80,60 @@ class SeleniumDriver:
 
     def add_tickets_to_cart(self, number_of_guests: int) -> bool:
         try:
-            add_main_ticket_element = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.element_to_be_clickable((By.XPATH,
-                                            "/html/body/app-root/app-layout/main/app-page-cart/div[2]/app-products-group/div/div/app-product-item[1]/div/div/div[2]/div/button[2]/i")))
+            add_main_ticket_element = self.wait_and_find_element(method=By.XPATH,
+                                                                 timeout=random.randrange(2, 5),
+                                                                 element_id_or_xpath="/html/body/app-root/app-layout/main/app-page-cart/div[2]/app-products-group/div/div/app-product-item[1]/div/div/div[2]/div/button[2]/i")
             add_main_ticket_element.click()
 
             for i in range(number_of_guests):
-                add_guest_element = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                    EC.element_to_be_clickable((By.XPATH,
-                                                "/html/body/app-root/app-layout/main/app-page-cart/div[2]/app-products-group/div/div/app-product-item[2]/div/div/div[2]/div/button[2]/i")))
+                add_guest_element = self.wait_and_find_element(method=By.XPATH,
+                                                               timeout=random.randrange(2, 5),
+                                                               element_id_or_xpath="/html/body/app-root/app-layout/main/app-page-cart/div[2]/app-products-group/div/div/app-product-item[2]/div/div/div[2]/div/button[2]/i")
+
                 add_guest_element.click()
 
             # continuar
-            continue_button = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.presence_of_element_located((By.ID, "buttonContinue")))
+            continue_button = self.wait_and_find_element(method=By.ID,
+                                                         timeout=random.randrange(2, 5),
+                                                         element_id_or_xpath="buttonContinue")
 
             self.driver.execute_script("arguments[0].click();", continue_button)
 
-            review_continue_button = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.presence_of_element_located((By.XPATH, "//button[@data-cy='review-button-continue']")))
+            review_continue_button = self.wait_and_find_element(method=By.XPATH,
+                                                                timeout=random.randrange(2, 5),
+                                                                element_id_or_xpath="//button[@data-cy='review-button-continue']")
 
             review_continue_button.click()
 
             # terms and conditions
-            checkbox = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(
-                EC.presence_of_element_located((By.ID, "tuPpEvent")))
+            checkbox = self.wait_and_find_element(method=By.ID,
+                                                  timeout=random.randrange(2, 5),
+                                                  element_id_or_xpath="tuPpEvent")
+
             checkbox.click()
 
-            review_continue_button = WebDriverWait(self.driver, timeout=random.randrange(2, 5)).until(  # find again
-                EC.presence_of_element_located((By.XPATH, "//button[@data-cy='review-button-continue']")))
+            review_continue_button = self.wait_and_find_element(method=By.XPATH,
+                                                                timeout=random.randrange(2, 5),
+                                                                element_id_or_xpath="//button[@data-cy='review-button-continue']")
 
             review_continue_button.click()
 
             return True
         except:
             print(
-                "erro ao adicionar adicionar (provavelmente esgotou). tentando outro setor.")
+                "erro ao adicionar ao carrinho (provavelmente \"esgotou\"). tentando outro setor.")
             self.driver.refresh()
 
     def log_in(self, user: str, password: str):
         try:
-            username_element = WebDriverWait(self.driver, timeout=10).until(
-                EC.presence_of_element_located((By.ID, "userLogin")))
+            username_element = self.wait_and_find_element(method=By.ID,
+                                                          timeout=15,
+                                                          element_id_or_xpath="userLogin")
             username_element.send_keys(user)
 
-            password_element = self.driver.find_element(By.ID, "password")
+            password_element = self.wait_and_find_element(method=By.ID,
+                                                          timeout=5,
+                                                          element_id_or_xpath="password")
             password_element.send_keys(password)
 
             password_element.submit()
@@ -131,3 +146,7 @@ class SeleniumDriver:
             print("deu ruim. deve estar dizendo que o produto não está disponível. tentando de novo...")
             self.driver.get(self.main_url)
             return False
+
+    def wait_and_find_element(self, method, timeout, element_id_or_xpath):
+        return WebDriverWait(self.driver, timeout=timeout).until(
+            EC.element_to_be_clickable((method, element_id_or_xpath)))
