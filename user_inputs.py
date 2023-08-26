4aca392ecd2160e66b381fb18f704a1133f4b908
Rafael Kamada <rafakamada@gmail.com>
2023-08-26 11:48:19 -0300

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -1,12 +1,21 @@
-# url da página de compra do jogo
-URL =
+from datetime import datetime
+
+# url da página de compra do jogo (tem que ser aquela que começa com "cart.totacesso")
+URL = "https://cart.totalacesso.com/saopaulofcxlducopaconmebolsudamericana_31_08_23"
 
 # lista com os setores em ordem de preferência
-GRANDSTAND_SECTIONS = ["ARQUIBANCADA BITSO LESTE", "ARQUIBANCADA OESTE", "ARQUIBANCADA NORTE", "ARQUIBANCADA SUL"]
+GRANDSTAND_SECTIONS = ["ARQUIBANCADA BITSO LESTE", "ARQUIBANCADA OESTE", "ARQUIBANCADA SUL"]
 
-#credenciais
-USERNAME =
-PASSWORD =
+# credenciais
+USERNAME = 
+PASSWORD = 
 
-# número de convidados 
-NUMBER_OF_GUESTS =
+# cpf do sócio
+CPF: int = 
+
+# número de convidados
+NUMBER_OF_GUESTS = 1
+
+# se quiser começar em um determinado horário
+IS_SCHEDULED = True
+SCHEDULED_TIMESTAMP = datetime(2023, 8, 24, 18, 00)
