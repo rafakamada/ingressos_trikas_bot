487dc0ec3cc528a9551e767055e631e86c0c6dda
Rafael Kamada <rafakamada@gmail.com>
2023-08-28 21:32:54 -0300

    dada a lista de setores desejados, identificar qual está disponível antes

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ driver = SeleniumDriver(URL, IS_SCHEDULED, SCHEDULED_TIMESTAMP)
 success = False
 section_index = 0
 
-
-def update_section_index(current_index):
-    if current_index + 1 < len(GRANDSTAND_SECTIONS):
-        return current_index + 1
-    else:
-        return 0
-
-
 while not success:
     driver.accept_cookies()
 
@@ -24,15 +16,18 @@ while not success:
     if not cpf_success:
         continue  # reiniciar loop
 
-    section_was_found = driver.go_to_section_tab(GRANDSTAND_SECTIONS[section_index])
+    target_section_found = driver.define_target_section(desired_sections=GRANDSTAND_SECTIONS)
 
-    if not section_was_found:
-        section_index = update_section_index(section_index)
+    if target_section_found == "none":
+        continue
+
+    target_section_tab_was_found = driver.go_to_section_tab(target_section_found)
+
+    if not target_section_tab_was_found:
         continue
 
     tickets_were_added = driver.add_tickets_to_cart(NUMBER_OF_GUESTS)
     if not tickets_were_added:
-        section_index = update_section_index(section_index)
         continue
 
     success = driver.log_in(USERNAME, PASSWORD)
